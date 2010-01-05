01aab4c350beeff910fb064b5d41a2344433fc47
Mikael Abrahamsson <mikael.abrahamsson@whitefish.se>
2010-01-05 11:31:56 +0100

    tweaks

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+if (!empty($_GET)) {
+	header('Content-type: text/javascript');
+	sleep(1);
+	echo "document.getElementById('log').innerHTML += 'Loaded: \'" . implode(' ', $_SERVER['argv']) .  "\'<br />'";
+	exit;
+}?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>jQuery.use</title>
+</head>
+<body>
+
+<div id="log"></div>
+	
+<script type="text/javascript" charset="utf-8" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script src="jquery.use.js" type="text/javascript" charset="utf-8"></script
+<script type="text/javascript" charset="utf-8">
+	/**
+	 * Set base url
+	 */
+	// jQuery.use.base = 'http://10.0.1.201/~mikaelabrahamsson/jquery/use';
+	
+	/**
+	 * Use a combine file as get args
+	 */
+	jQuery.use.combine = true;
+	
+	/**
+	 * Setup the modules
+	 */
+	jQuery.use.modules = {
+		'test1'	: { 'path': 'assets/scripts/test1.js' },
+		'test2'	: { 'path': 'assets/scripts/test2.js', 'require': 'test1' },
+		'test3'	: { 'path': 'assets/scripts/test3.js', 'require': ['test1', 'test2'] },
+		'test4'	: { 'path': 'assets/scripts/test4.js' },
+		'test5'	: { 'path': 'assets/scripts/test5.js', 'require': ['test4'] }
+	};
+	
+	/**
+	 * This should only load the 'test1'
+	 */	
+	jQuery.use('test1', function($){
+		console.log('LOADED: "test1"');
+	});
+	
+	/**
+	 * This should only load the 'test2' module, as the 'test1' module should allready be loaded
+	 */
+	jQuery.use('test2', function($){
+		console.log('LOADED: "test2"');
+	});
+	
+	/**
+	 * This should fire off as soon the earlier 'test2' use is loaded
+	 */
+	jQuery.use('test2', function($){
+		console.log('LOADED: "test2"');
+	});
+	
+	/**
+	 * This should only load the 'test3' module, as 'test1', 'test2' modules should allready be loaded
+	 */	
+	jQuery.use('test3', function($){
+		console.log('LOADED: "test3"');
+	});
+	
+	/**
+	 * This should only load both the the 'test4' and 'test5' module, 'test5' requires 'test4'
+	 */
+	jQuery.use('test5', function($){
+		console.log('LOADED: "test5"');
+	});
+	
+	/**
+	 * This should fire off as soon as the earlier 'test3' and 'test5' are done
+	 */
+	jQuery.use(['test3', 'test5'], function($){
+		console.log('LOADED: "test3, test5"');
+	});
+	
+	/**
+	 * This should fire off directly, uses no modules
+	 */
+	jQuery.use(function($){
+		console.log('LOADED: "none"');
+	});
+</script>
+
+</body>
+</html>
--- jquery.use.js
+++ jquery.use.js
@@ -1,151 +1,154 @@
-jQuery.extend({
-	use: (function() {
-			var $ = jQuery,
-				bDOMReady = false,
-				bWorking = false,
-				aLoad = [],
-				aLoaded = [];
+(function($) {
 	
-			function getBaseUrl() {
-				if (!jQuery.use.base) {
-					jQuery.use.base = window.location.protocol + '//' + window.location.hostname + window.location.pathname;
+	var bDOMReady = false,
+		bWorking = false,
+		aLoad = [],
+		aLoaded = [];
+	
+	function getBaseUrl() {
+		if (!$.use.base) {
+			$.use.base = window.location.protocol + '//' + window.location.hostname + window.location.pathname;
+		}
+		return $.use.base;
+	}
+
+	function getModules() {
+		return $.use.modules || [];
+	}
+
+	function getCombine() {
+		return $.use.combine || false;
+	}
+	
+	function build(aParams, aOld) {
+		var oModules = getModules(),
+			oModule,
+			aModules = aOld || [],
+			sParam,
+			path,
+			i,
+			iLen;
+
+		if ($.isArray(aParams[0])) {
+			aParams = aParams[0];
+		} else if (!$.isArray(aParams)) {
+			aParams = [aParams];
+		}
+
+		for (i=0, iLen = aParams.length; i < iLen; i++) {
+			sParam = aParams[i];
+			if (oModules.hasOwnProperty(sParam)) {
+				oModule = oModules[sParam];
+				path = oModule.path;
+
+				if (oModule.require) {
+					aModules = build([oModule.require], aModules);
 				}
-				return jQuery.use.base;
-			}
-	
-			function getModules() {
-				return jQuery.use.modules || [];
-			}
-			
-			function getCombine() {
-				return jQuery.use.combine || false;
-			}
-	
-			function build(aParams, aOld) {
-				var oModules = getModules(),
-					oModule,
-					aModules = aOld || [],
-					sParam,
-					path,
-					i,
-					iLen;
-		
-				if ($.isArray(aParams[0])) {
-					aParams = aParams[0];
-				} else if (!$.isArray(aParams)) {
-					aParams = [aParams];
+
+				if ($.inArray(path, aModules) === -1 && $.inArray(path, aLoaded) === -1) {
+					aModules.push(path);
 				}
-		
-				for (i=0, iLen = aParams.length; i < iLen; i++) {
-					sParam = aParams[i];
-					if (oModules.hasOwnProperty(sParam)) {
-						oModule = oModules[sParam];
-						path = oModule.path;
-												
-						if (oModule.require) {
-							aModules = build([oModule.require], aModules);
-						}
-				
-						if ($.inArray(path, aModules) === -1 && $.inArray(path, aLoaded) === -1) {
-							aModules.push(path);
-						}
-					}
-				}
-		
-				return aModules;
 			}
+		}
+
+		return aModules;
+	}
 	
-			function loader(aParams, fCallback) {
-		
-				var aModules = build(aParams);
-		
-				if (aModules.length === 0) {
+	function loader(aParams, fCallback) {
+
+		var aModules = build(aParams);
+
+		if (aModules.length === 0) {
+			$(fCallback);
+			next();
+			return;
+		}
+
+		setTimeout(function(){
+			var base = getBaseUrl(),
+				sModule,
+				iFiles = ((getCombine()) ? 1 : aModules.length),
+				aGetStr,
+				fCb;
+
+			if (iFiles === 1) {
+				sModule = aModules;
+				aGetStr = base + '?' + aModules.join('&');
+
+				fCb = function(){
 					$(fCallback);
 					next();
-					return;
-				}
-		
-				setTimeout(function(){
-					var base = getBaseUrl(),
-						sModule,
-						iFiles = ((getCombine()) ? 1 : aModules.length),
-						aGetStr,
-						fCb;
-					
-					if (iFiles === 1) {
-						sModule = aModules;
-						aGetStr = base + '?' + aModules.join('&');
-						
-						fCb = function(){
-							$(fCallback);
-							next();
-						};
+				};
+			} else {
+
+				sModule = [aModules.shift()];
+				aGetStr = base + '?' + sModule[0];
+
+				fCb = function(){
+					if (aModules.length === 0) {
+						$(fCallback);
+						next();
 					} else {
-						
-						sModule = [aModules.shift()];
-						aGetStr = base + '?' + sModule[0];
-						
-						fCb = function(){
-							if (aModules.length === 0) {
-								$(fCallback);
-								next();
-							} else {
-								var sModule = [aModules.shift()],
-									aGetStr = base + '?' + sModule[0];
-								
-								defer(aGetStr, fCb, sModule);
-							}
-						};
+						var sModule = [aModules.shift()],
+							aGetStr = base + '?' + sModule[0];
+
+						defer(aGetStr, fCb, sModule);
 					}
-					
-					defer(aGetStr, fCb, sModule);
-				}, 0);
+				};
 			}
-			
-			function defer(aGetStr, callback, aAddToLoaded) {
-				var elScript = document.createElement('script');
 
-				elScript.type = 'text/javascript';
-				elScript.src = aGetStr;
-				
-				$(elScript).one('load', function(){
-					aLoaded = aLoaded.concat(aAddToLoaded);
-					elScript.parentNode.removeChild(elScript);
-					callback();
-				});
+			defer(aGetStr, fCb, sModule);
+		}, 0);
+	}
 
-				document.body.appendChild(elScript);
-			}
-			
-			function next() {
-				bWorking = false;
-				var oNext = aLoad.shift();
-				if (oNext) {
-					setTimeout(function(){
-						get.apply(window, [oNext.params, oNext.callback]);
-					}, 0);
-				}
-			}
+	function defer(aGetStr, callback, aAddToLoaded) {
+		var elScript = document.createElement('script');
+
+		elScript.type = 'text/javascript';
+		elScript.src = aGetStr;
+
+		$(elScript).one('load', function(){
+			aLoaded = aLoaded.concat(aAddToLoaded);
+			elScript.parentNode.removeChild(elScript);
+			callback();
+		});
+
+		document.body.appendChild(elScript);
+	}
+
+	function next() {
+		bWorking = false;
+		var oNext = aLoad.shift();
+		if (oNext) {
+			setTimeout(function(){
+				get.apply(window, [oNext.params, oNext.callback]);
+			}, 0);
+		}
+	}
+
+	function get(aParams, fCallback) {
+		if (bWorking) {
+			aLoad.push({ 'params': aParams, 'callback': fCallback });
+		} else {
+			bWorking = true;
+			loader(aParams, fCallback);
+		}
+	}
 	
-			function get(aParams, fCallback) {
-				if (bWorking) {
-					aLoad.push({ 'params': aParams, 'callback': fCallback });
-				} else {
-					bWorking = true;
-					loader(aParams, fCallback);
-				}
-			}
+	function use() {
+		var aParams = $.makeArray(arguments),
+		fCallback = aParams.pop();
+
+		if (aParams.length === 0) {
+			$(fCallback);
+			return;
+		}
+
+		get(aParams, fCallback);
+	}
 	
-			return function () {
-				var aParams = $.makeArray(arguments),
-					fCallback = aParams.pop();
-		
-				if (aParams.length === 0) {
-					$(fCallback);
-					return;
-				}
-		
-				get(aParams, fCallback);
-			};
-		})()
-});
+	$.extend({
+		'use': use
+	});
+	
+})(jQuery);
